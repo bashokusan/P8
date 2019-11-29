18d184141a4aceb50f23f42d42c746cbbdc79dc2
Pierre S <44944658+bashokusan@users.noreply.github.com>
2019-11-29 14:57:58 +0100

    restrictions pour la suppresion de tache (#18)
    * restrictions pour la suppresion de tache
    
    * voter without switch

---
+++ src/Security/Voter/TaskVoter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class TaskVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        return $attribute === 'TASK_DELETE'
+            && $subject instanceof \App\Entity\Task;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if(null == $subject->getAuthor()){
+            return in_array('ROLE_ADMIN', $user->getRoles());
+        }
+
+        return $subject->getAuthor() == $user;
+    }
+}
