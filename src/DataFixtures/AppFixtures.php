16fbcb850860eaa85180c3ea315514eacfa5a7df
bashokusan <pierresimonnet1@gmail.com>
2020-01-07 12:17:38 +0100

    fixtures patch

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
@@ -18,18 +19,31 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        $users =[];
+
         $user = new User();
         $user->setUsername('user');
         $user->setPassword($this->encoder->encodePassword($user, 'password'));
         $user->setEmail('user@user.com');
         $user->setRoles(['ROLE_USER']);
         $manager->persist($user);
+        $users[] = $user;
 
-        $user->setUsername('admin');
-        $user->setPassword($this->encoder->encodePassword($user, 'password'));
-        $user->setEmail('admin@admin.com');
-        $user->setRoles(['ROLE_ADMIN']);
-        $manager->persist($user);
+        $user2 = new User();
+        $user2->setUsername('admin');
+        $user2->setPassword($this->encoder->encodePassword($user, 'password'));
+        $user2->setEmail('admin@admin.com');
+        $user2->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user2);
+        $users[] = $user2;
+
+        for ($p = 1; $p < 5; $p++){
+            $task = new Task();
+            $task->setTitle('task n-'.mt_rand());
+            $task->setContent("fake content of the task");
+            $task->setAuthor($users[array_rand($users)]);
+            $manager->persist($task);
+        }
 
         $manager->flush();
     }
